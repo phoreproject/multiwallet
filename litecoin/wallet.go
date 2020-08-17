1e6081ab72292ee4c791aea0aebcfc8a6f7784ea
anchaj <anchaj@users.noreply.github.com>
2020-08-17 20:35:39 +0200

    Adjust phore wallet code to a new wallet interface.

--- litecoin/wallet.go
+++ litecoin/wallet.go
@@ -27,7 +27,6 @@ import (
 	"github.com/phoreproject/multiwallet/model"
 	"github.com/phoreproject/multiwallet/service"
 	"github.com/phoreproject/multiwallet/util"
-	"github.com/op/go-logging"
 	"github.com/tyler-smith/go-bip39"
 	"golang.org/x/net/proxy"
 )
--- util/fees.go
+++ util/fees.go
@@ -54,7 +54,7 @@ func NewFeeProvider(maxFee, priorityFee, normalFee, economicFee, superEconomicFe
 func (fp *FeeProvider) GetFeePerByte(feeLevel wallet.FeeLevel) uint64 {
 	defaultFee := func() uint64 {
 		switch feeLevel {
-		case wallet.PRIOIRTY:
+		case wallet.PRIORITY:
 			return fp.priorityFee
 		case wallet.NORMAL:
 			return fp.normalFee
@@ -77,7 +77,7 @@ func (fp *FeeProvider) GetFeePerByte(feeLevel wallet.FeeLevel) uint64 {
 
 	var target FeeTargetInUSDCents
 	switch feeLevel {
-	case wallet.PRIOIRTY:
+	case wallet.PRIORITY:
 		target = PriorityTarget
 	case wallet.NORMAL:
 		target = NormalTarget
